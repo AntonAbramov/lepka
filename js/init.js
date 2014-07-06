b47f72347a8ff2f18461c3153ed374fe51b1949f
anton <AntonAbramov1990@gmail.com>
2014-07-06 19:01:07 +0300

    first pages

---
+++ js/init.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {
+	(function(){
+		var $grid = $('#grid');
+
+	$grid.shuffle({
+		itemSelector: '.picture-item'
+	});
+
+	$grid.on('done.shuffle', function () {
+		console.log('Finished initializing shuffle!');
+	});
+
+// Initialize shuffle
+	setTimeout(function () {
+		var opts = {
+			by: function ($el) {
+				return $el.data('title').toLowerCase();
+			}
+
+		}
+		$grid.shuffle('sort', opts);
+		console.log('alert')
+	}, 2000)
+	})();
+
+	(function(){
+		var $grid = $('#grid2');
+
+	$grid.shuffle({
+		itemSelector: '.picture-item'
+	});
+
+	$grid.on('done.shuffle', function () {
+		console.log('Finished initializing shuffle!');
+	});
+
+// Initialize shuffle
+	setTimeout(function () {
+		var opts = {
+			by: function ($el) {
+				return $el.data('title').toLowerCase();
+			}
+
+		}
+		$grid.shuffle('sort', opts);
+		console.log('alert')
+	}, 2000)
+	})()
+
+
+});
+
+$(window).resize(function () {
+
+});
+
+$(window).load(function () {
+
+
+});
+
+
+var popup = function () {
+	$(".overlay").on("click", function () {
+		$(this).fadeOut();
+		$('.popup').fadeOut();
+	})
+	$(".popup").find(".close").on("click", function () {
+		$(".overlay").fadeOut();
+		$('.popup').fadeOut();
+	});
+
+}
+var tabs = function () {
+	var tab = $(".tabs");
+	if (tab.length) {
+		tab.each(function () {
+
+			var idx = $(this).find(".tab-nav .active").index();
+			$(this).find('.tab-content .tab').hide().eq(idx).show();
+		});
+
+
+		tab.find(".tab-nav a").click(function (event) {
+			event.preventDefault();
+
+			if ($(this).hasClass("active")) {
+				return false;
+			} else {
+				$(this).parents('.tabs').find(".tab-nav a").removeClass('active');
+				var idx = $(this).addClass("active").index();
+				$(this).parents('.tabs').find(".tab-content .tab").hide();
+				$(this).parents('.tabs').find(".tab-content .tab").eq(idx).show();
+			}
+		});
+	}
+}
+
+
